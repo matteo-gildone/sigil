9914354846aaba7ed42a69ea8c00b34d5aafac6f
matteo-gildone <matteo.gildone@springernature.com>
2026-03-01 21:30:03 +0000

    feat(card3): add tests

--- internal/xdg/xdg_test.go
+++ internal/xdg/xdg_test.go
@@ -29,3 +29,31 @@ func TestDataDirNoEnvDir(t *testing.T) {
 		t.Errorf("want: %q, got: %q", want, got)
 	}
 }
+
+func TestConfigDirEnvDir(t *testing.T) {
+	t.Setenv("XDG_CONFIG_HOME", "/my/random/path")
+	want := "/my/random/path/sigil"
+	got, err := ConfigDir()
+
+	if err != nil {
+		t.Fatalf("expected no errors, got: %v", err)
+	}
+
+	if got != want {
+		t.Errorf("want: %q, got: %q", want, got)
+	}
+}
+
+func TestConfigDirNoEnvDir(t *testing.T) {
+	tempHome := t.TempDir()
+	t.Setenv("HOME", tempHome)
+	t.Setenv("XDG_CONFIG_HOME", "")
+	want := tempHome + "/.config/sigil"
+	got, err := ConfigDir()
+	if err != nil {
+		t.Fatalf("expected no errors, got: %v", err)
+	}
+	if got != want {
+		t.Errorf("want: %q, got: %q", want, got)
+	}
+}
