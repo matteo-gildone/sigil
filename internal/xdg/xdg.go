1ea05f3a655575e05f75334299b3763a1bad0470
matteo-gildone <matteo.gildone@springernature.com>
2026-03-01 21:09:49 +0000

    feat(card3): better file concatenation

--- internal/xdg/xdg.go
+++ internal/xdg/xdg.go
@@ -10,30 +10,34 @@ func DataDir() (string, error) {
 	if os.Getenv("XDG_DATA_HOME") == "" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
-			return "", fmt.Errorf("DataDir(): failed to get user home directory: %w", err)
+			return "", fmt.Errorf("failed to get user home directory: %w", err)
 		}
-		return filepath.Clean(homeDir) + "/.local/share/sigil", nil
+		return filepath.Join(filepath.Clean(homeDir), ".local", "shared", "sigil"), nil
 	}
 
-	return os.Getenv("XDG_DATA_HOME") + "/.local/share/sigil", nil
+	return filepath.Join(os.Getenv("XDG_DATA_HOME"), "sigil"), nil
 }
 
 func ConfigDir() (string, error) {
 	if os.Getenv("XDG_CONFIG_HOME") == "" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
-			return "", fmt.Errorf("ConfigDir(): failed to get user home directory: %w", err)
+			return "", fmt.Errorf("failed to get user home directory: %w", err)
 		}
-		return filepath.Clean(homeDir) + "/.config/sigil", nil
+		return filepath.Join(filepath.Clean(homeDir), ".config", "sigil"), nil
 	}
 
-	return os.Getenv("XDG_CONFIG_HOME") + "/.config/sigil", nil
+	return filepath.Join(os.Getenv("XDG_CONFIG_HOME"), "sigil"), nil
 }
 
 func ProjectPath(project string) (string, error) {
 	dataDir, err := DataDir()
 	if err != nil {
-		return "", fmt.Errorf("ProjectPath(): failed to get data directory: %w", err)
+		return "", fmt.Errorf("failed to get data directory: %w", err)
 	}
-	return fmt.Sprintf("%s/%s", dataDir, project), nil
+	path := fmt.Sprintf("%s/%s", dataDir, project)
+	if err := os.MkdirAll(path, 700); err != nil {
+		return "", fmt.Errorf("failed to create directory: %w", err)
+	}
+	return path, nil
 }
