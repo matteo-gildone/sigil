cbc84ca75340d9397279d828ab53fafd0fd7cf80
matteo-gildone <matteo.gildone@springernature.com>
2026-03-14 09:53:19 +0000

    feat(get): added get command

--- internal/command/get.go
+++ internal/command/get.go
@@ -3,6 +3,11 @@ package command
 import (
 	"flag"
 	"fmt"
+	"os"
+
+	"github.com/matteo-gildone/sigil/internal/cli"
+	"github.com/matteo-gildone/sigil/internal/store"
+	"github.com/matteo-gildone/sigil/internal/xdg"
 )
 
 var GetCmd = &Command{
@@ -16,10 +21,24 @@ func runGet(args []string) error {
 	project := getSubcommand.String("project", "default", "project namespace")
 	getSubcommand.Parse(args)
 
-	fmt.Println("'get' command is running")
+	passphrase, err := cli.PromptPassphrase("passphrase:", int(os.Stdin.Fd()))
+	if err != nil {
+		return fmt.Errorf("failed to read passphrase: %w", err)
+	}
 
-	if *project != "" {
-		fmt.Printf("on project %q\n", *project)
+	path, err := xdg.ProjectPath(*project)
+	if err != nil {
+		return fmt.Errorf("failed to read project path: %w", err)
+	}
+
+	s, err := store.Load(path, string(passphrase))
+	if err != nil {
+		return fmt.Errorf("failed to load store: %w", err)
+	}
+
+	k, v := s.Get(getSubcommand.Arg(0))
+	if v {
+		fmt.Println(k)
 	}
 	return nil
 }
